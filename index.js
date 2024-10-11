911e0582154ebdbbb488f2d84373964b21e61138
Neetesh Keshari <neet.kes@gmail.com>
2024-10-11 16:34:03 +0530

    Update index.js envo

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
