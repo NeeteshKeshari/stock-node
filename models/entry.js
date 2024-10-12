92cf3ea77fd8c792f3eb9bad299c30efdc15a9aa
Neetesh Keshari <neet.kes@gmail.com>
2024-10-12 20:20:48 +0530

    Update entry.js

--- models/entry.js
+++ models/entry.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const entrySchema = new mongoose.Schema({
   productName: { type: String },
   quantity: { type: Number },
+  number: { type: Number },
   date: { type: Date }
 }, { strict: false });
 
