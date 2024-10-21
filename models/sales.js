ce992bb18e667bb97cf713e05701f71ecd6da0b4
Neetesh Keshari <neet.kes@gmail.com>
2024-10-21 12:25:24 +0530

    Update sales.js

--- models/sales.js
+++ models/sales.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 
 const salesSchema = new mongoose.Schema({
-  product: { type: String },
-  quantity: { type: Number },
-  cost: { type: Number },
-  date: { type: Date },
-  customerName: { type: String },
-  customerAddress: { type: String },
-  amountPaid: { type: Number },
-  amountDue: { type: Number },
-  totalDue: { type: Number }
+  product: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  cost: { type: Number, required: true },
+  date: { type: Date, required: true },
+  customerName: { type: String, required: true },
+  customerAddress: { type: String, required: true },
+  amountPaid: [
+    {
+      amount: { type: Number, required: true },
+      date: { type: Date, required: true }
+    }
+  ],
+  amountDue: { type: Number, required: true },
+  totalDue: { type: Number, required: true }
 }, { strict: false });
 
 module.exports = mongoose.model('Sales', salesSchema);
