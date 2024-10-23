36cab81c45bc6f7492cbe61782abeb6117fb4f79
Neetesh Keshari <neet.kes@gmail.com>
2024-10-23 18:00:22 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  type: { // Add this field explicitly if it's missing
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
