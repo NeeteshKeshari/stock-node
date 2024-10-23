b9c9bf99c4298384dda874417bfad8699a54c186
Neetesh Keshari <neet.kes@gmail.com>
2024-10-23 17:58:56 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   selectedProduct: { type: String, required: true },
-  quantity: { type: Number },
-  actualCost: { type: Number },
-  sellingCost: { type: Number },
+  quantity: { type: Number, required: true }, // Set quantity to required
+  actualCost: { type: Number, required: true }, // Set actualCost to required
+  sellingCost: { type: Number, required: true } // Set sellingCost to required
 }, { strict: false });
 
 module.exports = mongoose.model('Product', productSchema);
